d806e8ae30517176e89e5b37a5f7ed7124d04e70
osaka2421 <osaka242128@gmail.com>
2026-03-05 00:36:27 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,44 @@
+# bitwise operator
+# and &
+a = 4
+b = 8
+print(a & b)
+
+# or |
+a= 7
+b = 4
+print(a | b)
+
+# ^ xor operator
+
+
+
+a = 4
+b= 7
+print(a ^ b)
+
+# << left shift
+
+print( 4 << 1)
+print ( 4 << 3)
+
+
+# >> right shift
+
+print(4>> 1)
+
+
+a= 6
+b = 10
+print( a ^ b)
+
+
+
+print(10<<2)
+print(10>>2)
+
+
+
+
+
+ 
